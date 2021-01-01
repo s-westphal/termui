d2e1427bda16dfae787073bc26248ae787424e7e
Susanne Westphal <westphal.susanne@gmx.de>
2021-01-01 19:39:35 +0100

    scatterplot

--- v3/widgets/plot.go
+++ v3/widgets/plot.go
@@ -23,6 +23,8 @@ type Plot struct {
 	DataLabels []string
 	MaxVal     float64
 	MinVal     float64
+	XMaxVal    float64
+	XMinVal    float64
 
 	LineColors []Color
 	AxesColor  Color // TODO
@@ -77,26 +79,29 @@ func NewPlot() *Plot {
 		PlotType:        LineChart,
 		MinVal:          math.Inf(1),
 		MaxVal:          math.Inf(-1),
+		XMinVal:         math.Inf(1),
+		XMaxVal:         math.Inf(-1),
 	}
 }
 
 func (self *Plot) renderBraille(buf *Buffer, drawArea image.Rectangle, minVal float64, maxVal float64) {
 	canvas := NewCanvas()
 	canvas.Rectangle = drawArea
+	xDx := MaxFloat64(1, self.XMaxVal-self.XMinVal)
 
 	switch self.PlotType {
 	case ScatterPlot:
-		for i, line := range self.Data {
-			for j, val := range line {
-				height := int((val - minVal) / (maxVal - minVal) * float64(drawArea.Dy()-1))
-				canvas.SetPoint(
-					image.Pt(
-						(drawArea.Min.X+(j*self.HorizontalScale))*2,
-						(drawArea.Max.Y-height-1)*4,
-					),
-					SelectColor(self.LineColors, i),
-				)
-			}
+		for i, x := range self.Data[0] {
+			y := self.Data[1][i]
+			height := int((y - minVal) / (maxVal - minVal) * float64(drawArea.Dy()-1))
+			canvas.SetPoint(
+				image.Pt(
+					(drawArea.Min.X+int((x-self.XMinVal)*float64(self.HorizontalScale*(drawArea.Dx()-1))/xDx))*2,
+					(drawArea.Max.Y-height-1)*4,
+				),
+				SelectColor(self.LineColors, 0),
+			)
+
 		}
 	case LineChart:
 		for i, line := range self.Data {
@@ -123,18 +128,19 @@ func (self *Plot) renderBraille(buf *Buffer, drawArea image.Rectangle, minVal fl
 }
 
 func (self *Plot) renderDot(buf *Buffer, drawArea image.Rectangle, minVal float64, maxVal float64) {
+	xDx := MaxFloat64(1, self.XMaxVal-self.XMinVal)
 	switch self.PlotType {
 	case ScatterPlot:
-		for i, line := range self.Data {
-			for j, val := range line {
-				height := int((val - minVal) / (maxVal - minVal) * float64(drawArea.Dy()-1))
-				point := image.Pt(drawArea.Min.X+(j*self.HorizontalScale), drawArea.Max.Y-1-height)
-				if point.In(drawArea) {
-					buf.SetCell(
-						NewCell(self.DotMarkerRune, NewStyle(SelectColor(self.LineColors, i))),
-						point,
-					)
-				}
+		for i, x := range self.Data[0] {
+			y := self.Data[1][i]
+			height := int((y - minVal) / (maxVal - minVal) * float64(drawArea.Dy()-1))
+			point := image.Pt(drawArea.Min.X+int((x-self.XMinVal)*float64(self.HorizontalScale*(drawArea.Dx()-1))/xDx), drawArea.Max.Y-1-height)
+			if point.In(drawArea) {
+				buf.SetCell(
+					NewCell(self.DotMarkerRune, NewStyle(SelectColor(self.LineColors, 0))),
+					point,
+				)
+
 			}
 		}
 	case LineChart:
@@ -171,24 +177,6 @@ func (self *Plot) plotAxes(buf *Buffer, minVal, maxVal float64) {
 			image.Pt(self.Inner.Min.X+yAxisLabelsWidth, i+self.Inner.Min.Y),
 		)
 	}
-
-	// draw rest
-	for x := self.Inner.Min.X + (yAxisLabelsWidth * self.HorizontalScale); x < self.Inner.Max.X-1; {
-		index := (x - (self.Inner.Min.X + yAxisLabelsWidth)) / (self.HorizontalScale)
-		label := fmt.Sprintf("%d", index)
-		if len(self.DataLabels) > index {
-			label = fmt.Sprintf(
-				"%s",
-				self.DataLabels[index],
-			)
-		}
-		buf.SetString(
-			label,
-			NewStyle(ColorWhite),
-			image.Pt(x, self.Inner.Max.Y-1),
-		)
-		x += (len(label) + xAxisLabelsGap) * self.HorizontalScale
-	}
 	// draw y axis labels
 	verticalScale := (maxVal - minVal) / float64(self.Inner.Dy()-xAxisLabelsHeight-1)
 	for i := 0; i*(yAxisLabelsGap+1) < self.Inner.Dy()-1; i++ {
@@ -197,6 +185,59 @@ func (self *Plot) plotAxes(buf *Buffer, minVal, maxVal float64) {
 			NewStyle(ColorWhite),
 			image.Pt(self.Inner.Min.X, self.Inner.Max.Y-(i*(yAxisLabelsGap+1))-2),
 		)
+	}
+	switch self.PlotType {
+	case ScatterPlot:
+		for _, x := range self.Data[0] {
+			self.XMinVal = MinFloat64(self.XMinVal, x)
+			self.XMaxVal = MaxFloat64(self.XMaxVal, x)
+		}
+
+		for x := self.Inner.Min.X + yAxisLabelsWidth; x < self.Inner.Max.X-1; {
+			index := (x - (self.Inner.Min.X + yAxisLabelsWidth)) / (self.HorizontalScale)
+			label := fmt.Sprintf("%.02f", self.XMinVal+(float64(index)*(self.XMaxVal-self.XMinVal)/float64(self.Inner.Dx()-yAxisLabelsWidth-1)))
+			if len(self.DataLabels) > index {
+				label = fmt.Sprintf(
+					"%s",
+					self.DataLabels[index],
+				)
+			}
+			buf.SetString(
+				label,
+				NewStyle(ColorWhite),
+				image.Pt(x, self.Inner.Max.Y-1),
+			)
+			x += (len(label) + xAxisLabelsGap) * self.HorizontalScale
+		}
+	case LineChart:
+		// draw x axis labels
+		// draw first label or 0
+		firstLabel := "0"
+		if len(self.DataLabels) > 0 {
+			firstLabel = self.DataLabels[0]
+		}
+		buf.SetString(
+			firstLabel,
+			NewStyle(ColorWhite),
+			image.Pt(self.Inner.Min.X+yAxisLabelsWidth, self.Inner.Max.Y-1),
+		)
+		// draw rest
+		for x := self.Inner.Min.X + yAxisLabelsWidth + (xAxisLabelsGap+len(firstLabel)-1)*self.HorizontalScale + 1; x < self.Inner.Max.X-1; {
+			index := int((x-(self.Inner.Min.X+yAxisLabelsWidth)-1)/(self.HorizontalScale) + 1)
+			label := fmt.Sprintf("%d", index)
+			if len(self.DataLabels) > index {
+				label = fmt.Sprintf(
+					"%s",
+					self.DataLabels[index],
+				)
+			}
+			buf.SetString(
+				label,
+				NewStyle(ColorWhite),
+				image.Pt(x, self.Inner.Max.Y-1),
+			)
+			x += (len(label) + xAxisLabelsGap) * self.HorizontalScale
+		}
 	}
 }
 
